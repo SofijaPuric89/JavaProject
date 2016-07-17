4b9de5be9e3fa4a059991563cbf37c13e0889825
vifili <filipovicviktorija@gmail.com>
2016-07-17 18:13:51 +0200

    Refactored Graph class to be part of transformer model

--- src/main/java/com/etf/rti/p1/transformer/BNFGrammarTransformer.java
+++ src/main/java/com/etf/rti/p1/transformer/BNFGrammarTransformer.java
@@ -1,10 +1,17 @@
 package com.etf.rti.p1.transformer;
 
+import com.etf.rti.p1.bnf.BNFCompiler;
+import com.etf.rti.p1.generator.AParser;
 import com.etf.rti.p1.transformer.rules.IRule;
 import com.etf.rti.p1.transformer.transformations.ITransform;
 import com.etf.rti.p1.transformer.transformations.TransformInnerOption;
 import com.etf.rti.p1.transformer.transformations.TransformRecursion;
+import org.antlr.works.Console;
 
+import java.io.ByteArrayInputStream;
+import java.io.FileOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -37,5 +44,32 @@ public class BNFGrammarTransformer {
             transformedRules.add(transformedRule);
         }
         return transformedRules;
+    }
+
+    /**
+     * TODO: try to refactor multiple usages of same code as in translateBnf method
+     * TODO: check if this is needed, and what this method should have as input/output
+     * @param grammar
+     * @throws Exception
+     */
+    public void transformToSyntaxDiagram(String grammar) throws Exception {
+        Path pathToGrammarFile = translateBnf(grammar);
+        String[] args = {"-f", pathToGrammarFile.toString(), "-o", "output", "-sd", "eps", "-verbose"};
+        Console.main(args);
+    }
+
+    private Path translateBnf(String input) throws Exception {
+        Path tmpDir = Files.createTempDirectory("test");
+        tmpDir.toFile().deleteOnExit();
+        final String name = "test";
+        Path grammarDirPath = tmpDir.resolve(name + ".g4");
+        String grammarName = grammarDirPath.toAbsolutePath().toString();
+        FileOutputStream out = new FileOutputStream(grammarName);
+
+        BNFCompiler compiler = new BNFCompiler(name, "com.etf.rti.p1.bnf", out);
+        compiler.setInput(new ByteArrayInputStream(input.getBytes("UTF-8")));
+        AParser parser = compiler.getParser();
+        parser.init();
+        return grammarDirPath.toAbsolutePath();
     }
 }
--- src/test/java/com/etf/rti/p1/transformer/BNFGrammarTransformerTest.java
+++ src/test/java/com/etf/rti/p1/transformer/BNFGrammarTransformerTest.java
@@ -25,6 +25,12 @@ public class BNFGrammarTransformerTest {
                     "<slovo> ::= a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z\n" +
                     "<cifra> ::= 0|1|2|3|4|5|6|7|8|9";
 
+    private static final String GRAMMAR_INPUT_TEST_2 =
+            "<start> ::= 11<a>|<b>1\n" +
+                    "<a> ::= 1|<a><b>|<a><c><b>\n" +
+                    "<b> ::= 101|<b>01\n" +
+                    "<c> ::= 1100|<c>11|<c>00\n";
+
     private BNFCompiler compiler;
     private BNFGrammarTransformer transformer;
 
@@ -49,5 +55,12 @@ public class BNFGrammarTransformerTest {
         assertEquals("kraj_domena = ( \"co.rs\" | \"com\" ).", rules.get(3).toString());
         assertEquals("rec = { slovo } slovo { cifra }.", rules.get(4).toString());
         assertEquals("slovo = ( \"a\" | \"b\" | \"c\" | \"d\" | \"e\" | \"f\" | \"g\" | \"h\" | \"i\" | \"j\" | \"k\" | \"l\" | \"m\" | \"n\" | \"o\" | \"p\" | \"q\" | \"r\" | \"s\" | \"t\" | \"u\" | \"v\" | \"w\" | \"x\" | \"y\" | \"z\" ).", rules.get(5).toString());
+    }
+
+    @Test
+    public void testTransformToSyntaxDiagram() throws Exception {
+        transformer.transformToSyntaxDiagram(GRAMMAR_INPUT_TEST_2);
+
+        //TODO: write some assertions when method is finally defined and implemented
     }
 }
