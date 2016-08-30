81097b3bac054c56331dd1deec9336ef282395a1
Viktorija Filipovic <vifi@zuehlke.com>
2016-08-30 11:53:01 +0200

    Refactored syntax highlighting classes.

--- src/main/java/com/etf/rti/p1/ui/highlight/BNFTokenMaker.java
+++ src/main/java/com/etf/rti/p1/ui/highlight/BNFTokenMaker.java
@@ -1,16 +1,11 @@
 package com.etf.rti.p1.ui.highlight;
 
-import org.fife.ui.rsyntaxtextarea.AbstractTokenMaker;
 import org.fife.ui.rsyntaxtextarea.Token;
 import org.fife.ui.rsyntaxtextarea.TokenMap;
-import org.fife.ui.rsyntaxtextarea.TokenTypes;
 
 import javax.swing.text.Segment;
 
-public class BNFTokenMaker extends AbstractTokenMaker {
-
-    private int currentTokenStart;
-    private int currentTokenType;
+public class BNFTokenMaker extends AbstractGrammarNotationTokenMaker {
 
     @Override
     public TokenMap getWordsToHighlight() {
@@ -24,132 +19,89 @@ public class BNFTokenMaker extends AbstractTokenMaker {
         tokenMap.put("[", Token.RESERVED_WORD);
         tokenMap.put("]", Token.RESERVED_WORD);
         tokenMap.put("|", Token.RESERVED_WORD);
-        /*tokenMap.put("<", Token.RESERVED_WORD);
-        tokenMap.put(">", Token.RESERVED_WORD);*/
 
         return tokenMap;
     }
 
     @Override
-    public Token getTokenList(Segment text, int startTokenType, int startOffset) {
-        resetTokenList();
-
-        char[] array = text.array;
-        int offset = text.offset;
-        int count = text.count;
-        int end = offset + count;
-
-        int newStartOffset = startOffset - offset;
-
-        currentTokenStart = offset;
-        currentTokenType = startTokenType;
-
-        for (int i = offset; i < end; i++) {
-            char c = array[i];
-
-            switch (currentTokenType) {
-                case Token.NULL:
-                    // Starting a new token
-                    currentTokenStart = i;
-                    switch (c) {
-                        case ' ':
-                        case '\t':
-                        case '\r':
-                        case '\n':
-                            currentTokenType = Token.WHITESPACE;
-                            break;
-
-                        case '<':
-                            // terminal symbol
-                            currentTokenType = Token.VARIABLE;
-                            break;
-
-                        default:
-                            currentTokenType = Token.IDENTIFIER;
-                    }
-                    break;
-
-                case Token.WHITESPACE:
-                    switch (c) {
-                        case ' ':
-                        case '\t':
-                        case '\r':
-                        case '\n':
-                            break;
-
-                        case '<':
-                            // nonterminal symbol
-                            addToken(text, currentTokenStart, i - 1, Token.WHITESPACE, newStartOffset + currentTokenStart);
-                            currentTokenStart = i;
-                            currentTokenType = Token.VARIABLE;
-                            break;
-
-                        default:
-                            addToken(text, currentTokenStart, i - 1, Token.WHITESPACE, newStartOffset + currentTokenStart);
-                            currentTokenStart = i;
-                            currentTokenType = Token.IDENTIFIER;
-                    }
-                    break;
-
-                case Token.VARIABLE:
-                    // nonterminal
-                    switch (c) {
-                        case '>':
-                            addToken(text, currentTokenStart, i, Token.VARIABLE, newStartOffset + currentTokenStart);
-                            currentTokenStart = i + 1;
-                            currentTokenType = Token.NULL;
-                            break;
-                    }
-                    break;
-
-                case Token.IDENTIFIER:
-                    switch (c) {
-                        case ' ':
-                        case '\t':
-                        case '\r':
-                        case '\n':
-                            addToken(text, currentTokenStart, i - 1, Token.IDENTIFIER, newStartOffset + currentTokenStart);
-                            currentTokenStart = i;
-                            currentTokenType = Token.WHITESPACE;
-                            break;
-                        case '|':
-                            addToken(text, currentTokenStart, i - 1, Token.IDENTIFIER, newStartOffset + currentTokenStart);
-                            addToken(text, i, i, Token.IDENTIFIER, newStartOffset + i);
-                            currentTokenStart = i + 1;
-                            currentTokenType = Token.NULL;
-                            break;
-                        case '<':
-                            addToken(text, currentTokenStart, i - 1, Token.IDENTIFIER, newStartOffset + currentTokenStart);
-                            currentTokenStart = i;
-                            currentTokenType = Token.VARIABLE;
-                            break;
-                    }
-            }
-        }
-
+    void handleCurrentToken(Segment text, int newStartOffset, int i, char c) {
         switch (currentTokenType) {
             case Token.NULL:
-                addNullToken();
+                // Starting a new token
+                currentTokenStart = i;
+                switch (c) {
+                    case ' ':
+                    case '\t':
+                    case '\r':
+                    case '\n':
+                        currentTokenType = Token.WHITESPACE;
+                        break;
+
+                    case '<':
+                        // terminal symbol
+                        currentTokenType = Token.VARIABLE;
+                        break;
+
+                    default:
+                        currentTokenType = Token.IDENTIFIER;
+                }
                 break;
 
-            // No continuing on new line
-            default:
-                addToken(text, currentTokenStart, end - 1, currentTokenType, newStartOffset + currentTokenStart);
-                addNullToken();
+            case Token.WHITESPACE:
+                switch (c) {
+                    case ' ':
+                    case '\t':
+                    case '\r':
+                    case '\n':
+                        break;
 
+                    case '<':
+                        // nonterminal symbol
+                        addToken(text, currentTokenStart, i - 1, Token.WHITESPACE, newStartOffset + currentTokenStart);
+                        currentTokenStart = i;
+                        currentTokenType = Token.VARIABLE;
+                        break;
+
+                    default:
+                        addToken(text, currentTokenStart, i - 1, Token.WHITESPACE, newStartOffset + currentTokenStart);
+                        currentTokenStart = i;
+                        currentTokenType = Token.IDENTIFIER;
+                }
+                break;
+
+            case Token.VARIABLE:
+                // nonterminal
+                switch (c) {
+                    case '>':
+                        addToken(text, currentTokenStart, i, Token.VARIABLE, newStartOffset + currentTokenStart);
+                        currentTokenStart = i + 1;
+                        currentTokenType = Token.NULL;
+                        break;
+                }
+                break;
+
+            case Token.IDENTIFIER:
+                switch (c) {
+                    case ' ':
+                    case '\t':
+                    case '\r':
+                    case '\n':
+                        addToken(text, currentTokenStart, i - 1, Token.IDENTIFIER, newStartOffset + currentTokenStart);
+                        currentTokenStart = i;
+                        currentTokenType = Token.WHITESPACE;
+                        break;
+                    case '|':
+                        addToken(text, currentTokenStart, i - 1, Token.IDENTIFIER, newStartOffset + currentTokenStart);
+                        addToken(text, i, i, Token.IDENTIFIER, newStartOffset + i);
+                        currentTokenStart = i + 1;
+                        currentTokenType = Token.NULL;
+                        break;
+                    case '<':
+                        addToken(text, currentTokenStart, i - 1, Token.IDENTIFIER, newStartOffset + currentTokenStart);
+                        currentTokenStart = i;
+                        currentTokenType = Token.VARIABLE;
+                        break;
+                }
         }
-
-        return firstToken;
-    }
-
-    @Override
-    public void addToken(Segment segment, int start, int end, int tokenType, int startOffset) {
-        if (tokenType == Token.IDENTIFIER) {
-            int value = wordsToHighlight.get(segment, start, end);
-            if (value != -1) {
-                tokenType = value;
-            }
-        }
-        super.addToken(segment, start, end, tokenType, startOffset);
     }
 }
--- src/main/java/com/etf/rti/p1/ui/highlight/EBNFTokenMaker.java
+++ src/main/java/com/etf/rti/p1/ui/highlight/EBNFTokenMaker.java
@@ -1,15 +1,11 @@
 package com.etf.rti.p1.ui.highlight;
 
-import org.fife.ui.rsyntaxtextarea.AbstractTokenMaker;
 import org.fife.ui.rsyntaxtextarea.Token;
 import org.fife.ui.rsyntaxtextarea.TokenMap;
 
 import javax.swing.text.Segment;
 
-public class EBNFTokenMaker extends AbstractTokenMaker {
-
-    private int currentTokenStart;
-    private int currentTokenType;
+public class EBNFTokenMaker extends AbstractGrammarNotationTokenMaker {
 
     @Override
     public TokenMap getWordsToHighlight() {
@@ -28,136 +24,96 @@ public class EBNFTokenMaker extends AbstractTokenMaker {
         return tokenMap;
     }
 
+
     @Override
-    public Token getTokenList(Segment text, int startTokenType, int startOffset) {
-        resetTokenList();
-
-        char[] array = text.array;
-        int offset = text.offset;
-        int count = text.count;
-        int end = offset + count;
-
-        int newStartOffset = startOffset - offset;
-
-        currentTokenStart = offset;
-        currentTokenType = startTokenType;
-
-        for (int i = offset; i < end; i++) {
-            char c = array[i];
-
-            switch (currentTokenType) {
-                case Token.NULL:
-                    // Starting a new token
-                    currentTokenStart = i;
-                    switch (c) {
-                        case ' ':
-                        case '\t':
-                        case '\r':
-                        case '\n':
-                            currentTokenType = Token.WHITESPACE;
-                            break;
-                        case '=':
-                        case '|':
-                        case '{':
-                        case '}':
-                        case '(':
-                        case ')':
-                        case '[':
-                        case ']':
-                        case '.':
-                            addToken(text, i, i, Token.IDENTIFIER, newStartOffset + i);
-                            currentTokenStart = i + 1;
-                            currentTokenType = Token.NULL;
-                            break;
-                        case '"':
-                            currentTokenType = Token.IDENTIFIER;
-                            currentTokenStart = i;
-                            break;
-                        default:
-                            currentTokenType = Token.VARIABLE;
-                    }
-                    break;
-
-                case Token.WHITESPACE:
-                    switch (c) {
-                        case ' ':
-                        case '\t':
-                        case '\r':
-                        case '\n':
-                            break;
-
-                        case '"':
-                            // terminal symbol
-                            addToken(text, currentTokenStart, i - 1, Token.WHITESPACE, newStartOffset + currentTokenStart);
-                            currentTokenStart = i;
-                            currentTokenType = Token.IDENTIFIER;
-                            break;
-
-                        default:
-                            addToken(text, currentTokenStart, i - 1, Token.WHITESPACE, newStartOffset + currentTokenStart);
-                            currentTokenStart = i;
-                            currentTokenType = Token.VARIABLE;
-                    }
-                    break;
-
-                case Token.VARIABLE:
-                    // nonterminal
-                    switch (c) {
-                        case '=':
-                        case '|':
-                        case '{':
-                        case '}':
-                        case '(':
-                        case ')':
-                        case '[':
-                        case ']':
-                        case '.':
-                            addToken(text, currentTokenStart, i - 1, Token.VARIABLE, newStartOffset + currentTokenStart);
-                            addToken(text, i, i, Token.IDENTIFIER, newStartOffset + i);
-                            currentTokenStart = i + 1;
-                            currentTokenType = Token.NULL;
-                            break;
-                        case '"':
-                            addToken(text, currentTokenStart, i - 1, Token.VARIABLE, newStartOffset + currentTokenStart);
-                            currentTokenStart = i;
-                            currentTokenType = Token.IDENTIFIER;
-                    }
-                    break;
-
-                case Token.IDENTIFIER:
-                    switch (c) {
-                        case '"':
-                            addToken(text, currentTokenStart, i, Token.IDENTIFIER, newStartOffset + currentTokenStart);
-                            currentTokenStart = i + 1;
-                            currentTokenType = Token.NULL;
-                            break;
-                    }
-            }
-        }
-
+    void handleCurrentToken(Segment text, int newStartOffset, int i, char c) {
         switch (currentTokenType) {
             case Token.NULL:
-                addNullToken();
+                // Starting a new token
+                currentTokenStart = i;
+                switch (c) {
+                    case ' ':
+                    case '\t':
+                    case '\r':
+                    case '\n':
+                        currentTokenType = Token.WHITESPACE;
+                        break;
+                    case '=':
+                    case '|':
+                    case '{':
+                    case '}':
+                    case '(':
+                    case ')':
+                    case '[':
+                    case ']':
+                    case '.':
+                        addToken(text, i, i, Token.IDENTIFIER, newStartOffset + i);
+                        currentTokenStart = i + 1;
+                        currentTokenType = Token.NULL;
+                        break;
+                    case '"':
+                        currentTokenType = Token.IDENTIFIER;
+                        currentTokenStart = i;
+                        break;
+                    default:
+                        currentTokenType = Token.VARIABLE;
+                }
                 break;
 
-            // No continuing on new line
-            default:
-                addToken(text, currentTokenStart, end - 1, currentTokenType, newStartOffset + currentTokenStart);
-                addNullToken();
+            case Token.WHITESPACE:
+                switch (c) {
+                    case ' ':
+                    case '\t':
+                    case '\r':
+                    case '\n':
+                        break;
 
+                    case '"':
+                        // terminal symbol
+                        addToken(text, currentTokenStart, i - 1, Token.WHITESPACE, newStartOffset + currentTokenStart);
+                        currentTokenStart = i;
+                        currentTokenType = Token.IDENTIFIER;
+                        break;
+
+                    default:
+                        addToken(text, currentTokenStart, i - 1, Token.WHITESPACE, newStartOffset + currentTokenStart);
+                        currentTokenStart = i;
+                        currentTokenType = Token.VARIABLE;
+                }
+                break;
+
+            case Token.VARIABLE:
+                // nonterminal
+                switch (c) {
+                    case '=':
+                    case '|':
+                    case '{':
+                    case '}':
+                    case '(':
+                    case ')':
+                    case '[':
+                    case ']':
+                    case '.':
+                        addToken(text, currentTokenStart, i - 1, Token.VARIABLE, newStartOffset + currentTokenStart);
+                        addToken(text, i, i, Token.IDENTIFIER, newStartOffset + i);
+                        currentTokenStart = i + 1;
+                        currentTokenType = Token.NULL;
+                        break;
+                    case '"':
+                        addToken(text, currentTokenStart, i - 1, Token.VARIABLE, newStartOffset + currentTokenStart);
+                        currentTokenStart = i;
+                        currentTokenType = Token.IDENTIFIER;
+                }
+                break;
+
+            case Token.IDENTIFIER:
+                switch (c) {
+                    case '"':
+                        addToken(text, currentTokenStart, i, Token.IDENTIFIER, newStartOffset + currentTokenStart);
+                        currentTokenStart = i + 1;
+                        currentTokenType = Token.NULL;
+                        break;
+                }
         }
-
-        return firstToken;
-    }
-
-    @Override
-    public void addToken(Segment segment, int start, int end, int tokenType, int startOffset) {
-        if (tokenType == Token.IDENTIFIER) {
-            int value = wordsToHighlight.get(segment, start, end);
-            if (value != -1) {
-                tokenType = value;
-            }
-        }
-        super.addToken(segment, start, end, tokenType, startOffset);
     }
 }
